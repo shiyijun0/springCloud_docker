4d10197369aa430237a54d4e61ed8dca638b93da
shiyijun <1178078767@qq.com>
2018-11-29 17:46:53 +0800

    提交注册服务

--- microservice-simple-provider-user/src/main/java/com/itmuch/cloud/study/ProviderUserApplication.java
+++ microservice-simple-provider-user/src/main/java/com/itmuch/cloud/study/ProviderUserApplication.java
@@ -2,8 +2,10 @@ package com.itmuch.cloud.study;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
+@EnableEurekaClient
 public class ProviderUserApplication {
 
     public static void main(String[] args) {
